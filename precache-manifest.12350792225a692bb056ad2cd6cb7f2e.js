3d41f72ebef63ee1b66c9e157c64ec3936e6d1ef
xibjrt <jvaiciulyte@gmail.com>
2020-03-29 23:00:24 +0300

    Updates

---
+++ precache-manifest.12350792225a692bb056ad2cd6cb7f2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77401eae7feab807c2eca73d95ed0f35",
+    "url": "/projektas/index.html"
+  },
+  {
+    "revision": "3dcf5d2b77df52df5b9e",
+    "url": "/projektas/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c65baeea8f2c51e2dcc6",
+    "url": "/projektas/static/js/2.2c60708e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projektas/static/js/2.2c60708e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dcf5d2b77df52df5b9e",
+    "url": "/projektas/static/js/main.5457e8bd.chunk.js"
+  },
+  {
+    "revision": "ac4923daa4cb6cbe2541",
+    "url": "/projektas/static/js/runtime-main.fd872c0a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/projektas/static/media/logo.5d5d9eef.svg"
+  }
+]);
